266ff3a39c57bd8369d7ae398f1c567fa56b412c
Kevin London <kevinlondon@gmail.com>
2013-12-26 14:06:28 -0800

    Exercise for Code Magnets in ch 5

---
+++ ch5/MultiFor.java
@@ -0,0 +1,12 @@
+class MultiFor {
+    public static void main(String[] args) {
+        for (int x = 0; x < 4; x++) {
+            for (int y = 4; y > 2; y--) {
+                System.out.println(x + " " + y);
+            }
+            if (x == 1) {
+                x++;
+            }
+        }
+    }
+}
