dd849da955510d6c4c8505a108948940533e7015
Kevin London <kevinlondon@gmail.com>
2013-12-26 19:11:55 -0800

    Add some example code plus enough to get it running for polymorphism examples.

---
+++ ch8/Animal.java
@@ -0,0 +1,3 @@
+public class Animal {
+    private String name;
+}
---
+++ ch8/AnimalTestDrive.java
@@ -0,0 +1,9 @@
+public class AnimalTestDrive{
+    public static void main (String[] args) {
+        MyAnimalList list = new MyAnimalList();
+        Dog a = new Dog();
+        Cat c = new Cat();
+        list.add(a);
+        list.add(c);
+    }
+}
