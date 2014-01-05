62912457ad678682df447f79f7d547bf97ee6fa3
Kevin London <kevinlondon@gmail.com>
2014-01-04 17:47:30 -0800

    Working on second gui example.

---
+++ ch12/SimpleGui1B.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class SimpleGui1B implements ActionListener {
+    JButton button;
+
+    public static void main (String[] args) {
+        SimpleGui1B gui = new SimpleGui1B();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("Click me");
+
+        // Register interest with the button. Add to list of listeners.
+        button.addActonListener(this);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        // Implement actionlistener interface actionPerformed method
+        button.setText("I've been clicked!");
+    }
+}
