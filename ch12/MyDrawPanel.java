0536b3fcef45b4254d991c4a81ebb7564ba92802
Kevin London <kevinlondon@gmail.com>
2014-01-05 01:03:43 -0800

    Add main method to drawpanel for testing.

--- ch12/MyDrawPanel.java
+++ ch12/MyDrawPanel.java
@@ -3,6 +3,14 @@ import java.awt.*;
 import javax.swing.*;
 
 class MyDrawPanel extends JPanel {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        MyDrawPanel panel = new MyDrawPanel();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(panel);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
 
     // Do not call directly. Handled by Java.
     public void paintComponent(Graphics g) {
