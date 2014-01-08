c01ccb7aa812e21abeb2432d2dcc288ed8ddc8f8
Kevin London <kevinlondon@gmail.com>
2014-01-08 00:32:00 -0800

    Version one of BeatBox app.

--- ch13/BeatBox.java
+++ ch13/BeatBox.java
@@ -40,6 +40,157 @@ public class BeatBox {
 
         JButton start = new JButton("Start");
         start.addActionListener(new MyStartListener());
+        buttonBox.add(start);
 
+        JButton stop = new JButton("Stop");
+        stop.addActionListener(new MyStopListener());
+        buttonBox.add(stop);
+
+        JButton upTempo = new JButton("Tempo Up");
+        upTempo.addActionListener(new MyUpTempoListener());
+        buttonBox.add(upTempo);
+
+        JButton downTempo = new JButton("Tempo Down");
+        downTempo.addActionListener(new MyDownTempoListener());
+        buttonBox.add(downTempo);
+
+        Box nameBox = new Box(BoxLayout.Y_AXIS);
+        for (int i = 0; i < 16; i++) {
+            nameBox.add(new Label(instrumentNames[i]));
+        }
+
+        background.add(BorderLayout.EAST, buttonBox);
+        background.add(BorderLayout.WEST, nameBox);
+
+        theFrame.getContentPane().add(background);
+        GridLayout grid = new GridLayout(16, 16);
+        grid.setVgap(1);
+        grid.setHgap(2);
+        mainPanel = new JPanel(grid);
+        background.add(BorderLayout.CENTER, mainPanel);
+
+        // make checkboxes, set to unchecked, add to list and gui panel
+        for (int i = 0; i < 256; i++) {
+            JCheckBox c = new JCheckBox();
+            c.setSelected(false);
+            checkBoxList.add(c);
+            mainPanel.add(c);
+        }
+
+        setUpMidi();
+
+        theFrame.setBounds(50, 50, 300, 300);
+        // Set center element to fit, others can deal with it.
+        theFrame.pack();
+        theFrame.setVisible(true);
     }
+
+
+    public void setUpMidi() {
+        // get sequencer, set up sequence and track
+        try {
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequence = new Sequence(Sequence.PPQ, 4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e) {e.printStackTrace();}
+    }
+
+
+    public void buildTrackAndStart() {
+        // Make 16 elem array for values of one instrument across 16 beats.
+        // Put in key if supposed to play, else 0.
+        int[] trackList = null;
+
+        // create fresh track, yo.
+        sequence.deleteTrack(track);
+        track = sequence.createTrack();
+
+        for (int i = 0; i < 16; i++) {
+            // do this per each instrument (ie 16 rows)
+            trackList = new int[16];
+
+            // set key which represents instrument
+            int key = instruments[i];
+
+            for (int j = 0; j < 16; j++) {
+                // do for each beat in measure
+                // is the check box selected? if not, add to list for track
+                JCheckBox jc = (JCheckBox) checkBoxList.get(j + (16 * i));
+                if ( jc.isSelected() ) {
+                    trackList[j] = key;
+                } else {
+                    trackList[j] = 0;
+                }
+            }
+
+            makeTracks(trackList);
+            track.add(makeEvent(176, 1, 127, 0, 16));
+        }
+
+        // always make sure that there is an event at beat 16.
+        track.add(makeEvent(192, 9, 1, 0, 15));
+        try {
+            sequencer.setSequence(sequence);
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+            sequencer.start();
+            sequencer.setTempoInBPM(120);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // First of the inner classes.
+    public class MyStartListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            buildTrackAndStart();
+        }
+    }
+
+    public class MyStopListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            sequencer.stop();
+        }
+    }
+
+    public class MyUpTempoListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            float tempoFactor = sequencer.getTempoFactor();
+            sequencer.setTempoFactor((float) (tempoFactor * 1.03));
+        }
+    }
+
+    public class MyDownTempoListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            float tempoFactor = sequencer.getTempoFactor();
+            sequencer.setTempoFactor((float) (tempoFactor * .97));
+        }
+    }
+
+
+    // makes events for one instrument at a time.
+    public void makeTracks(int[] list) {
+        for (int i = 0; i < 16; i++) {
+            int key = list[i];
+            if (key != 0) {
+                // make on and off events, add to track
+                track.add(makeEvent(144, 9, key, 100, i));
+                track.add(makeEvent(128, 9, key, 100, i+1));
+            }
+        }
+    }
+
+
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception e) {e.printStackTrace();}
+        return event;
+    }
+
 }
