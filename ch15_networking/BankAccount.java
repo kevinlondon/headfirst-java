58751c29e3ec2ee5d3c66f0c0484c789dfafc723
Kevin London <kevinlondon@gmail.com>
2014-01-13 22:34:07 -0800

    Ryan and Monica example, simple threading.

---
+++ ch15_networking/BankAccount.java
@@ -0,0 +1,11 @@
+class BankAccount {
+    private int balance = 100;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        balance = balance - amount;
+    }
+}
---
+++ ch15_networking/RyanAndMonicaExample.java
@@ -0,0 +1,40 @@
+public class RyanAndMonicaExample implements Runnable {
+    private BankAccount account = new BankAccount();
+
+    public static void main (String[] args) {
+        RyanAndMonicaExample example = new RyanAndMonicaExample();
+        Thread one = new Thread(example);
+        Thread two = new Thread(example);
+        one.setName("Ryan");
+        two.setName("Monica");
+        one.start();
+        two.start();
+    }
+
+    public void run() {
+        for (int x = 0; x < 10; x++) {
+            makeWithdrawl(10);
+            if (account.getBalance() < 0) {
+                System.out.println("Overdrawn!");
+            }
+        }
+    }
+
+    private void makeWithdrawl(int amount) {
+        String name = Thread.currentThread().getName();
+        if (account.getBalance() >= amount) {
+            System.out.println(name + " will withdraw.");
+            try {
+                System.out.println(name + " falling asleep.");
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println(name + " woke up.");
+            account.withdraw(amount);
+            System.out.println(name + " completes Withdrawl");
+        } else {
+            System.out.println("Sorry, not enough for " + name);
+        }
+    }
+}
