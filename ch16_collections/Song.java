7d79644587f2d49bb276826fd111c5eae1dce5c8
Kevin London <kevinlondon@gmail.com>
2014-01-15 20:37:56 -0800

    Jukebox3 example from ch 16. Doesn't yet compile.

---
+++ ch16_collections/Song.java
@@ -0,0 +1,34 @@
+class Song {
+    String title;
+    String artist;
+    String rating;
+    String bpm;
+
+    Song(String t, String a, String r, String bpm) {
+        title = t;
+        artist = a;
+        rating = r;
+        bpm = bpm;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+    public String toString() {
+        // Override toString() so we see the title when printing a list.
+        return title;
+    }
+}
