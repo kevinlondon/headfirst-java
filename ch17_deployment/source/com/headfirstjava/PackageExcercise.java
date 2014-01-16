6f7608f9788838fcb3be5c96b8470d973bb5f338
Kevin London <kevinlondon@gmail.com>
2014-01-15 22:45:32 -0800

    Create into a jar using jar -cvmf manifest.txt packEx.jar com from classes folder.

--- ch17_deployment/source/com/headfirstjava/PackageExcercise.java
+++ ch17_deployment/source/com/headfirstjava/PackageExcercise.java
@@ -4,4 +4,10 @@ import javax.swing.*;
 
 public class PackageExcercise {
     // life altering code~
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
