27492cec2f1be17c9e5bc7a78df5d2bf7f1b642f
Kevin London <kevinlondon@gmail.com>
2014-01-18 13:58:48 -0800

    Add completed code example for rmi interface

--- ch18_remote_interface/MyRemote.java
+++ ch18_remote_interface/MyRemote.java
@@ -1,8 +1,9 @@
 import java.rmi.*;
 
-// Interface announces it extends java.rmi.Remote
+// Interface announces it extends java.rmi.Remote (which is allowed)
 public interface MyRemote extends Remote {
     // RemoteException may happen with rmi
     // Return type must be serializable or primitve
+    // If your own class will be passed, make it serializable too.
     public String sayHello() throws RemoteException;
 }
--- ch18_remote_interface/MyRemoteImpl.java
+++ ch18_remote_interface/MyRemoteImpl.java
@@ -1,6 +1,27 @@
+import java.rmi.*;
+// server contains UnicastRemoteObject
+import java.rmi.server.*;
+
+// Simplest way to perform some work as a remote service object
 public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
+    // Must implement custom method we've specified in interface
     public String sayHello() {
         return "Server says, 'Hey'";
     }
-    // more code
+
+    // UnicastRemoteObject requires we catch RemoteException
+    public MyRemoteImpl() throws RemoteException { }
+
+    public static void main (String[] args) {
+
+        try {
+            // Make the remote object and bind it to the rmiregistry
+            // The name you register it under is the name clients will need
+            // to look it up under in the rmi registry
+            MyRemote service = new MyRemoteImpl();
+            Naming.rebind("Remote Hello", service);
+        } catch (Exception ex) }
+            ex.printStackTrace();
+        }
+    }
 }
